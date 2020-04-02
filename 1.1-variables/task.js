35f51f3786ded7e06a0fa401f309dfef3ab17151
Ruslan Suyunbaev <suyunbaev.r.u@yandex.ru>
2020-04-02 14:56:08 +0400

    Базовые понятия

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -6,22 +6,42 @@
  */
 
 function averageMark(a, g, p) {
+	let algebra = a;
+	let geography = g;
+	let physics = p
+	let averageMark; 
 	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	averageMark = (algebra + geography + physics) / 3; 
+	
+	return averageMark.toFixed(2);
 }
 
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myname = userName;
+	let messege;
+	message = `Привет, мир! Меня зовут ${myname}.`
+
+	return message;
 }
 
+
 function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+	let result;
+	result = x * y + 5 * z + x - 1 ; 
+
+    return result;
 }
+
+
+
+/* ВТОРОЙ Вариант задачи 3//
+function calculateFormula(x, y, z) {
+	let result;
+	result = x * y + 5 * z + x - 1 ; 
+
+    return result;
+}
+calculateFormula(2, 22, 0); */
