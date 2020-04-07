6a8e04b273edd6eef9b5c6a4fcadbd5e4f6dba34
Ruslan Suyunbaev <suyunbaev.r.u@yandex.ru>
2020-04-08 00:54:08 +0400

    HomeWork 2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,44 @@
+'use strict';
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let x = [];
+    let d = b ** 2 - 4 * a * c;
+
+	if (d === 0) {
+        x[0] = (-b + Math.sqrt(d)) / (2 * a);   
+    }else if (d > 0) {
+	    x[0] = (-b + Math.sqrt(d)) / (2 * a);
+	    x[1] = (-b - Math.sqrt(d)) / (2 * a);
+    }
+    
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
-}
+    let averageMark = 0;
+    let i = 0;
+    let sumPoints = 0;
+    let marksFix = marks.slice(0, 5);
+    if (marks.length == 0) {
+      return 0; 
+    } 
+  
+    
+    while (i < marksFix.length) {
+        sumPoints = sumPoints + marksFix[i];
+        i++;
+    }averageMark = sumPoints / marksFix.length; 
+    
+    return averageMark;
+  }
+
+
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let year = new Date().getFullYear();
+	let age = year - dateOfBirthday.getFullYear();
+	if (age < 18) {
+		return ("Сожалею, " + name + ", но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!")
+	} 
+	return ("Не желаете ли олд-фэшн, " + name + "?")
+  
 }
