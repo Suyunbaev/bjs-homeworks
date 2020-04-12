fdefc10ffa4108e8b75ba1a64b9a072f13e4b88b
Ruslan Suyunbaev <suyunbaev.r.u@yandex.ru>
2020-04-12 13:11:10 +0400

    Задание 3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,44 @@
+'use strict';
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let z = new Date();
+    let period = date.getMonth() - z.getMonth();
+    let loanAmount = parseFloat(amount) - parseFloat(contribution);
+    let s = loanAmount;
+    let p = parseFloat(percent) / 12 / 100;
+    let totalAmount;
+    let monthlyPayment;
+    let n;
+    let i;
+    if (z >= date) {
+        return console.log("Вы ввели не верную дату");
+    }
+    if (isNaN(p)) {
+	    return "Не корректное значение процентной ставки";
+    }
+    if (isNaN(contribution)) {
+	   return "Не корректное значение первоначального взноса"; 
+    }
+    if (isNaN(amount)) {
+	    return "Не корректное значение суммы кредита"; 
+    }
+    period = parseFloat(period);
+    i = date.getFullYear() - z.getFullYear();
+    if (i > 0){
+      period = period + i*12;
+    }
+    n = period;
+    monthlyPayment = s*(p+p/(((1+p)**n)-1));
+    monthlyPayment = parseFloat(monthlyPayment);
+    totalAmount = monthlyPayment * period;
+    return parseFloat(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
-}
+    let greeting;
+    if (name == !!0 , name == undefined)
+        {greeting = "Привет, мир! Меня зовут Аноним";
+    }else {
+        greeting = `Привет, мир! Меня зовут ${name}`
+    }
+    return greeting;
+}   
