64badd9c5ed52b90989988ee7cf4c7b577bdf3f5
Ruslan Suyunbaev <suyunbaev.r.u@yandex.ru>
2020-04-19 18:04:03 +0400

    Quest 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,37 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    if (animal === undefined) {
+        return null;}
+    else{
+      let sound = animal.sound;
+      return sound;
+    }
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks == null || marks == undefined || marks == Number.isNaN()){
+        return 0;
+    }
+    let average = 0;
+    for (let i = 0; i < marks.length; i++){
+        average += marks[i];
+    }
+    average = average / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    if (typeof(birthday) !== typeof(new Date())){
+      return "Извините, но по закону мы обязаны проверить Ваши документы"
+    }
+    let now = new Date();
+    birthday = new Date(birthday);
+    let diff = now - birthday;
+    let age = diff / 31556952000;
+    console.log(age);
+    if (age > 18) {
+        return "Что Вам налить?"
+    }else{
+        return "Извините, мы не продаем алкоголь несовершеннолетним "
+    }
 }
